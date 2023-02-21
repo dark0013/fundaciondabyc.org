c0e3e06f37d4c82846b476a12d5cfcf4a7e9aeb5
Alain Campoverde <andresarcangel00@gmail.com>
2023-02-21 16:14:58 -0500

    cambios rutas

--- page/master/header.php
+++ page/master/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilo_admin.css">
-    <link rel="stylesheet" href="css/carga.css">
-    <link rel="stylesheet" href="module/DataTables/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="module/boostrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/estilo_admin.css">
+    <link rel="stylesheet" href="../../css/carga.css">
+    <link rel="stylesheet" href="../../module/DataTables/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="../../module/boostrap/css/bootstrap.min.css">
     <title>Administrador | DABYC</title>
 </head>
 <body>
