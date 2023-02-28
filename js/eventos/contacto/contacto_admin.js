29c93cf16b5fd263f88da3a20a5e616f45893666
Roberto Coello <roberto88esau@hotmail.com>
2023-02-28 01:31:03 -0500

    contacto

--- js/eventos/contacto/contacto_admin.js
+++ js/eventos/contacto/contacto_admin.js
@@ -1,11 +1,11 @@
 let idTabla = 0;
 
 
-const btnEnviarMensaje = document.getElementById("btn_Enviar_Mensaje");
+const btnENVIARMENSAJE = document.getElementById("btn_Enviar_Mensaje");
 
 btnEnviarMensaje.addEventListener("click", async (e) => {
     e.preventDefault();//
-    //const identificacion_card = document.getElementById("txt_identificacion_card").value;//
+    const identification_card = document.getElementById("txt_cedula").value;
     const full_name = document.getElementById("txt_nombre_completo").value;
     const email = document.getElementById("txt_correo_electronico").value;
     const phone = document.getElementById("txt_numero_telefonico").value;
@@ -15,7 +15,43 @@ btnEnviarMensaje.addEventListener("click", async (e) => {
     const user_sesion = "ALCAMPOVERDE";
     const usur_creation = "ALCAMPOVERDE";
 
-    let parametros = JSON.stringify({  full_name,
+    let parametros = JSON.stringify({ identification_card, full_name,
+         email,phone,direccion,address,description,status,user_sesion,usur_creation });
+
+    console.log(parametros);
+
+    let data = await fetch('http://localhost/ApiFundacionDabyc/controllers/contactos', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json ',
+            'Content-Type': 'application/json'
+        },
+        body: parametros
+    })
+
+    if (data.status == 200) {
+        alert('EXITO al guardar, refresque pantalla o presione F5 para poder previsualizar los cambios');
+        limpiarCampos();
+        location.reload();
+    }
+
+});
+
+const btnSUBSCRIBIRSE = document.getElementById("btn_subscrubirse");
+
+btnSUBSCRIBIRSE.addEventListener("click", async (e) => {
+    e.preventDefault();//
+    const identification_card = document.getElementById("txt_cedula").value;
+    const full_name = document.getElementById("txt_nombre_completo").value;
+    const email = document.getElementById("txt_correo_electronico").value;
+    const phone = document.getElementById("txt_numero_telefonico").value;
+    const address = document.getElementById("txt_direccion").value;
+    const description = document.getElementById("txt_escribe_tu_mensaje").value;
+    const status = document.getElementById("txt_escribe_tu_mensaje").value;
+    const user_sesion = "ALCAMPOVERDE";
+    const usur_creation = "ALCAMPOVERDE";
+
+    let parametros = JSON.stringify({ identification_card, full_name,
          email,phone,direccion,address,description,status,user_sesion,usur_creation });
 
     console.log(parametros);
