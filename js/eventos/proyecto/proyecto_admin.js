ab708281b35362a72a17870cb02ebd0d22d2d225
FranklirvS <franklinriofrioespa16@gmail.com>
2023-03-06 23:10:47 -0500

    cambios en vistas proyectosç

---
+++ js/eventos/proyecto/proyecto_admin.js
@@ -0,0 +1,193 @@
+
+let idTabla = 0;
+
+
+const btnGuardar = document.getElementById("btn_guardar");
+
+btnGuardar.addEventListener("click", async (e) => {
+    e.preventDefault();//
+    const title= document.getElementById("title").value;
+    const url_image = document.getElementById("file").value;
+    const description= document.getElementById("description").value;
+    const date_creation= document.getElementById("date").value;
+    const status = "A";
+    const user_sesion = "ALCAMPOVERDE";
+    const user_creation = "ALCAMPOVERDE";
+    let parametros = JSON.stringify({ title,url_image,description,date_creation,status, user_sesion, user_creation,});
+
+    console.log(parametros);
+
+    let data = await fetch('http://localhost/ApiFundacionDabyc/controllers/proyectos', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json ',
+            'Content-Type': 'application/json'
+        },
+        body: parametros
+    })
+
+    if (data.status == 200) {
+        alert('exito al guardar, refresque pantalla o presione F5 para poder previsualizar los cambios');
+        limpiarCampos();
+        location.reload();
+    }
+
+});
+
+
+const btnEditar = document.getElementById("btn_editar");
+btnEditar.addEventListener("click", async (e) => {
+    e.preventDefault();//
+    const id_project  = document.getElementById("txt_id_proyect").value;
+    const title= document.getElementById("title").value;
+    const url_image = document.getElementById("file").value;
+    const description= document.getElementById("description").value;
+    const date_creation= document.getElementById("date").value;
+    const user_sesion = "ALCAMPOVERDE";
+    const user_update = "ALCAMPOVERDE";
+
+    let parametros = JSON.stringify({ id_project,title,url_image,description,date_creation, user_sesion, user_update});
+
+    console.log(parametros);
+
+    let data = await fetch('http://localhost/ApiFundacionDabyc/controllers/proyectos', {
+        method: 'PUT',
+        headers: {
+            'accept': 'application/json ',
+            'Content-Type': 'application/json'
+        },
+        body: parametros
+    })
+
+    if (data.status == 200) {
+        alert('exito al guardar, refresque pantalla o presione F5 para poder previsualizar los cambios');
+        limpiarCampos();
+        location.reload();
+    }
+
+});
+
+
+
+
+const btnInactivar = document.getElementById("btn_inactivar");
+btnInactivar.addEventListener("click", async (e) => {
+    e.preventDefault();//
+
+    alert("Inactivar");
+    const id_project= document.getElementById("txt_id_proyect").value;
+
+    /*  const user_update = "ALCAMPOVERDE";
+     const usur_creation = "ALCAMPOVERDE"; */
+
+    let parametros = JSON.stringify({ id_project });
+
+    console.log(parametros);
+
+    let data = await fetch('http://localhost/ApiFundacionDabyc/controllers/proyectos', {
+        method: 'DELETE',
+        headers: {
+            'accept': 'application/json ',
+            'Content-Type': 'application/json'
+        },
+        body: parametros
+    })
+
+    if (data.status == 200) {
+        alert('exito al guardar, refresque pantalla o presione F5 para poder previsualizar los cambios');
+        limpiarCampos();
+        location.reload();
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+const getData = async () => {
+    const data = await fetch('http://localhost/ApiFundacionDabyc/controllers/proyectos?page');
+    console.log(data);
+    if (data.status === 200) {
+        const datos = await data.json();
+        console.log(datos);
+        llenarTabla("tblDatos1", datos)
+    }
+}
+
+
+function llenarTabla(tabla, filas) {
+    $('#' + tabla + ' tbody').remove();
+    $('#' + tabla).dataTable().fnDestroy();
+    $('#' + tabla).DataTable({
+        language: {
+            search: "Buscar",
+            info: "Total: _TOTAL_ registros. ",
+            emptyTable: "No hay información.",
+            infoEmpty: "Total: 0 registros.",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            select: { rows: " %d filas seleccionadas." }
+        },
+        select: true,
+        scrollY: 300,
+        scrollX: true,
+        scrollCollapse: true,
+        paging: false,
+        fixedColumns: false,
+        data: filas,
+        columns: [
+            { data: 'id_project' },
+            { data: 'title' },
+            { data: 'url_image' },
+            { data: 'description' },
+            { data: 'date_creation' },
+            { data: 'status' },
+            
+        ],
+        columnDefs: [{
+            targets: [], //OCULTAR COLUMNAS
+            visible: false,
+            searchable: true
+        }]
+    });
+
+    //MANEJO DE EVENTO DE LA TABLA
+    $('#' + tabla + ' tbody').on('click', 'tr', function () {
+
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+            document.getElementById("txt_id_proyect").value;
+            document.getElementById("title").value;
+            document.getElementById("description").value;
+            document.getElementById("date").value;
+        } else {
+            let cellData = $('#' + tabla).DataTable().row($(this)).data();
+
+            idTabla = cellData.id_project;
+            document.getElementById("txt_id_proyect").value= cellData.id_project;
+            document.getElementById("title").value = cellData.title;
+            document.getElementById("description").value = cellData.description;
+            document.getElementById("date").value = cellData.date_creation;
+
+        }
+
+    });
+}
+
+
+window.onload = () => { getData(); };
+
+
+const limpiarCampos = _ => {
+    document.getElementById("title").value = " ";
+    document.getElementById("file").file = new File([]);
+    document.getElementById("description").value = " ";
+    document.getElementById("date").value = " ";
+}
--- page/proyecto/proyecto_admin.php
+++ page/proyecto/proyecto_admin.php
@@ -9,18 +9,38 @@
     </div>
     <div class="container">
         <form action="action_page.php">
+            <input type="hidden" id="txt_id_proyect">
             <label for="title">Titulo:</label>
             <input type="text" id="title" name="title" placeholder="Título..">
             <label for="date">Fecha:</label>
-            <input type="date" id="date" name="date" placeholder="Fecha..">
+            <input type="datetime-local" id="date" name="date" placeholder="Fecha..">
             <label for="description">Descripción:</label>
             <textarea id="description" name="description" placeholder="Descripción.." style="height:200px"></textarea>
             <label for="file">Archivo:</label>
             <input type="file" id="file" name="file" placeholder="Archivo..">
-            <input type="submit" value="Enviar">
+            <input type="button" id="btn_guardar" value="Guardar">
+            <input type="button" id="btn_editar" value="ACTUALIZAR" >
+            <input type="button" id="btn_inactivar" value="Inactivar">
+
         </form>
     </div>
 </setion>
+<section id="tabla">
+<table id="tblDatos1"  class="table table-striped table-bordered" style="width: 100%">
+      <thead>
+        <tr >
+          <th>ID</th>
+          <th>title</th>
+          <th>url_image</th>
+          <th>description</th>
+          <th>date_creation</th>
+          <th>status</th>
+        </tr>
+      </thead>
+    </table>
+</section>
+
+
 
 <script src="../../js/eventos/proyecto/proyecto_admin.js"></script>
 <?php include('../master/script.php') ?>
